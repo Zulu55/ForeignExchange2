f651d74d8b401a2a64d4d909b8be730650ab6f0d
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2017-08-16 20:40:38 -0500

    Cration initial project

---
+++ ForeignExchange2/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace ForeignExchange2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ForeignExchange2Page();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ForeignExchange2/ForeignExchange2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ForeignExchange2
+{
+    public partial class ForeignExchange2Page : ContentPage
+    {
+        public ForeignExchange2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
